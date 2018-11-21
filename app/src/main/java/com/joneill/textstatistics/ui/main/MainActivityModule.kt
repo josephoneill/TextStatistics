82209fb307f8ae835bc09c3c4af87b2c7648845e
josephoneill <devjoseph97@gmail.com>
2018-11-21 00:06:08 -0500

    Downgraded Daagger version to 2.16

--- app/src/main/java/com/joneill/textstatistics/ui/main/MainActivityModule.kt
+++ app/src/main/java/com/joneill/textstatistics/ui/main/MainActivityModule.kt
@@ -1,13 +1,9 @@
 package com.joneill.textstatistics.ui.main
 
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.joneill.textstatistics.ui.main.interactor.MainInteractor
 import com.joneill.textstatistics.ui.main.interactor.MainMVPInteractor
 import com.joneill.textstatistics.ui.main.presenter.MainMVPPresenter
-import com.joneill.textstatistics.ui.main.presenter.HomePresenter
-import com.joneill.textstatistics.ui.home.view.ContactsAdapter
 import com.joneill.textstatistics.ui.main.presenter.MainPresenter
-import com.joneill.textstatistics.ui.main.view.MainActivity
 import com.joneill.textstatistics.ui.main.view.MainMVPView
 import dagger.Module
 import dagger.Provides
