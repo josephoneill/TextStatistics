6728f90147126145a7548eb9f5b19d68cd56a7fb
josephoneill <devjoseph97@gmail.com>
2018-11-30 00:21:20 -0500

    Added MessagesRepository to Interactor, removed and added functions

--- app/src/main/java/com/joneill/textstatistics/ui/home/interactor/HomeInteractor.kt
+++ app/src/main/java/com/joneill/textstatistics/ui/home/interactor/HomeInteractor.kt
@@ -1,26 +1,27 @@
 package com.joneill.textstatistics.ui.main.interactor
 
-import android.graphics.BitmapFactory
 import com.joneill.textstatistics.data.preferences.PreferenceHelper
 import com.joneill.textstatistics.ui.base.interactor.BaseInteractor
 import javax.inject.Inject
-import android.content.ContentResolver
 import android.content.Context
-import android.net.Uri
+import com.joneill.textstatistics.data.repository.MessagesRepository
 import com.joneill.textstatistics.data.text.Contact
+import com.joneill.textstatistics.data.text.Message
 import com.joneill.textstatistics.data.text.TextDataHelper
 
 
-class HomeInteractor @Inject internal constructor(preferenceHelper: PreferenceHelper, context : Context, private val textDataHelper : TextDataHelper) : BaseInteractor(preferenceHelper = preferenceHelper), HomeMVPInteractor {
+class HomeInteractor @Inject internal constructor(preferenceHelper: PreferenceHelper, context : Context, private val messagesRepository: MessagesRepository) : BaseInteractor(preferenceHelper = preferenceHelper), HomeMVPInteractor {
+    override fun loadData() = messagesRepository.loadData()
 
-    override fun getTextData(): List<Contact>? {
-        return textDataHelper.getContacts()
-    }
+    override fun getContacts(): List<Contact> = messagesRepository.contacts
 
-    override fun filterCreatorsContactsOnly(contacts : List<Contact>?): List<Contact>? {
+    override fun getConversations(contacts : List<Contact>): List<Message> = messagesRepository.messages
+
+    override fun getMessagesByContact(messages: List<Message>, contact: Contact): List<Message> = messagesRepository.getMessagesByContact(contact)
+
+    override fun setCurrentContact(contact : String?) = preferenceHelper.setCurrentContact(contact)
+
+    /*override fun filterCreatorsContactsOnly(contacts : List<Contact>?): List<Contact>? {
         return textDataHelper.filterCreatorsContactsOnly(contacts)
-    }
-
-    /*override fun getUserDetails() = Pair(preferenceHelper.getCurrentUserName(),
-            preferenceHelper.getCurrentUserEmail())*/
+    }*/
 }
--- app/src/main/java/com/joneill/textstatistics/ui/home/interactor/HomeMVPInteractor.kt
+++ app/src/main/java/com/joneill/textstatistics/ui/home/interactor/HomeMVPInteractor.kt
@@ -1,9 +1,14 @@
 package com.joneill.textstatistics.ui.main.interactor
 
 import com.joneill.textstatistics.data.text.Contact
+import com.joneill.textstatistics.data.text.Message
 import com.joneill.textstatistics.ui.base.interactor.MVPInteractor
+import io.reactivex.Completable
 
 interface HomeMVPInteractor : MVPInteractor {
-    fun getTextData() : List<Contact>?
-    fun filterCreatorsContactsOnly(contacts : List<Contact>?) : List<Contact>?
+    fun loadData() : Completable
+    fun getContacts() : List<Contact>
+    fun getConversations(contacts : List<Contact>) : List<Message>
+    fun getMessagesByContact(messages : List<Message>, contact : Contact) : List<Message>
+    fun setCurrentContact(contact : String?)
 }
