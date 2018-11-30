fdf34dd77e6dd33206a8274731a076e1e16bf057
josephoneill <devjoseph97@gmail.com>
2018-11-30 00:16:20 -0500

    Made the Contact data class a Parcelable

--- app/src/main/java/com/joneill/textstatistics/data/text/Contact.kt
+++ app/src/main/java/com/joneill/textstatistics/data/text/Contact.kt
@@ -1,5 +1,8 @@
 package com.joneill.textstatistics.data.text
 
 import android.graphics.Bitmap
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
-data class Contact constructor(val name: String, val number: String, val profileImage : Bitmap?)
+@Parcelize
+data class Contact constructor(val name: String, val number: String, val profileImage : Bitmap?) : Parcelable
