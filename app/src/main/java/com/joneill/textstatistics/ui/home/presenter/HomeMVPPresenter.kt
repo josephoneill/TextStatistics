7f9b06c2b5ec7b543367460de3d292a85cd69770
josephoneill <devjoseph97@gmail.com>
2018-11-30 00:21:46 -0500

    Added function for item on click

--- app/src/main/java/com/joneill/textstatistics/ui/home/presenter/HomeMVPPresenter.kt
+++ app/src/main/java/com/joneill/textstatistics/ui/home/presenter/HomeMVPPresenter.kt
@@ -3,9 +3,10 @@ package com.joneill.textstatistics.ui.main.presenter
 import com.joneill.textstatistics.ui.base.presenter.MVPPresenter
 import com.joneill.textstatistics.ui.home.view.HomeMVPView
 import com.joneill.textstatistics.ui.main.interactor.HomeMVPInteractor
-import com.joneill.textstatistics.ui.main.interactor.MainMVPInteractor
-import com.joneill.textstatistics.ui.main.view.MainMVPView
+import com.joneill.textstatistics.data.text.Contact
+
 
 interface HomeMVPPresenter<V : HomeMVPView, I : HomeMVPInteractor> : MVPPresenter<V, I> {
     fun onViewPrepared()
+    fun onContactItemClick(contact : Contact)
 }
