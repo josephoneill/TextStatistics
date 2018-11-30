30a0838d5aa7574c16b1be4d6d2124ff4e437176
josephoneill <devjoseph97@gmail.com>
2018-11-30 00:16:59 -0500

    Added new functions to get Messages

--- app/src/main/java/com/joneill/textstatistics/data/text/TextDataHelper.kt
+++ app/src/main/java/com/joneill/textstatistics/data/text/TextDataHelper.kt
@@ -1,8 +1,9 @@
 package com.joneill.textstatistics.data.text
 
 interface TextDataHelper {
-    fun getAllConversations(): List<String>?
-    fun getAllCreators() : List<Contact>?
-    fun getContacts() : List<Contact>?
-    fun filterCreatorsContactsOnly(contacts : List<Contact>?) : List<Contact>?
+    fun getAllConversations(contacts : List<Contact>): List<Message>
+    fun getContacts() : List<Contact>
+    fun filterCreatorsContactsOnly(contacts : List<Contact>?) : List<Contact>
+    fun getContactByNumber(contacts : List<Contact>, number : String?) : Contact?
+    fun getMessagesByContact(messages : List<Message>, contact : Contact?) : List<Message>
 }
