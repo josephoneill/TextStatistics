6194abcfa7c2736589e9349b9d51a22b6ca05f61
josephoneill <devjoseph97@gmail.com>
2018-11-30 00:16:42 -0500

    Renamed data class Text to Message

--- app/src/main/java/com/joneill/textstatistics/data/text/Message.kt
+++ app/src/main/java/com/joneill/textstatistics/data/text/Message.kt
@@ -1,3 +1,3 @@
 package com.joneill.textstatistics.data.text
 
-data class Text constructor(val data: String, val contact : Contact)
+data class Message constructor(val data: String?, val contact : Contact?)
