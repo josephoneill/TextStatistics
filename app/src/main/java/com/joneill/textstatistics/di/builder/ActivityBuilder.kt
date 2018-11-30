88c8a4d7ac31ed95c5c1fc0bfbafa0ce2d0c05d9
josephoneill <devjoseph97@gmail.com>
2018-11-30 00:17:38 -0500

    Added ContactDataFragment to modules

--- app/src/main/java/com/joneill/textstatistics/di/builder/ActivityBuilder.kt
+++ app/src/main/java/com/joneill/textstatistics/di/builder/ActivityBuilder.kt
@@ -1,5 +1,6 @@
 package com.joneill.textstatistics.di.builder
 
+import com.joneill.textstatistics.ui.home.ContactDataFragmentProvider
 import com.joneill.textstatistics.ui.home.HomeFragmentProvider
 import com.joneill.textstatistics.ui.main.MainActivityModule
 import com.joneill.textstatistics.ui.main.view.MainActivity
@@ -8,6 +9,6 @@ import dagger.android.ContributesAndroidInjector
 
 @Module
 abstract class ActivityBuilder {
-    @ContributesAndroidInjector(modules = [(MainActivityModule::class), (HomeFragmentProvider::class)])
+    @ContributesAndroidInjector(modules = [(MainActivityModule::class), (HomeFragmentProvider::class), (ContactDataFragmentProvider::class)])
     abstract fun bindMainActivity(): MainActivity
 }
