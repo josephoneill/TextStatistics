1758bb1a172948bf2301909f9996b305ace7647f
josephoneill <devjoseph97@gmail.com>
2019-01-17 23:45:09 -0500

    Added new Message methods

--- app/src/main/java/com/joneill/textstatistics/data/text/Message.kt
+++
@@ -1,3 +0,0 @@
-package com.joneill.textstatistics.data.text
-
-data class Message constructor(val data: String?, val contact : Contact?)
--- app/src/main/java/com/joneill/textstatistics/data/text/TextDataHelper.kt
+++ app/src/main/java/com/joneill/textstatistics/data/text/TextDataHelper.kt
@@ -1,9 +1,17 @@
 package com.joneill.textstatistics.data.text
 
+import com.joneill.textstatistics.data.text.data.Contact
+import com.joneill.textstatistics.data.text.data.Message
+import java.util.*
+
 interface TextDataHelper {
     fun getAllConversations(contacts : List<Contact>): List<Message>
     fun getContacts() : List<Contact>
     fun filterCreatorsContactsOnly(contacts : List<Contact>?) : List<Contact>
     fun getContactByNumber(contacts : List<Contact>, number : String?) : Contact?
     fun getMessagesByContact(messages : List<Message>, contact : Contact?) : List<Message>
+    fun getMessagesByDate(messages : List<Message>, date : Date) : List<Message>
+    fun getMessageCountOnDate(messages : List<Message>, date : Date) : Int
+    fun getMessageCountByDate(messages : List<Message>) : Map<String, Int>
+    fun getMessagesInDateRange(messages : List<Message>, startDate : Date, endDate : Date) : List<Message>
 }
