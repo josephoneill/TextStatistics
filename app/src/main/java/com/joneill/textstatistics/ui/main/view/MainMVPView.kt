156f8ddfa0ca786b2ceab03d2113b137b5cb7a96
josephoneill <devjoseph97@gmail.com>
2019-01-17 23:50:50 -0500

    Removed unused imports

--- app/src/main/java/com/joneill/textstatistics/ui/main/view/MainMVPView.kt
+++ app/src/main/java/com/joneill/textstatistics/ui/main/view/MainMVPView.kt
@@ -1,8 +1,6 @@
 package com.joneill.textstatistics.ui.main.view
 
 import android.app.Activity
-import android.content.Context
-import com.joneill.textstatistics.data.text.Contact
 import com.joneill.textstatistics.ui.base.view.MVPView
 
 interface MainMVPView : MVPView {
