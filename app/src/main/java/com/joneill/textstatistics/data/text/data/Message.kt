51299b5e94f0b2da4be6909414335e94d3b372f0
josephoneill <devjoseph97@gmail.com>
2019-01-17 23:31:40 -0500

    Created Message data class

---
+++ app/src/main/java/com/joneill/textstatistics/data/text/data/Message.kt
@@ -0,0 +1,5 @@
+package com.joneill.textstatistics.data.text.data
+import com.joneill.textstatistics.data.text.data.Contact
+import java.util.*
+
+data class Message constructor(val data: String?, val contact : Contact?, val date : Date)
