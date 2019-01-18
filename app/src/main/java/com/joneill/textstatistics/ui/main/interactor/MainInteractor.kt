a8ea33e998475cd0d65cdb9d8286256f3e8e7617
josephoneill <devjoseph97@gmail.com>
2019-01-17 23:50:35 -0500

    Removed unused imports

--- app/src/main/java/com/joneill/textstatistics/ui/main/interactor/MainInteractor.kt
+++ app/src/main/java/com/joneill/textstatistics/ui/main/interactor/MainInteractor.kt
@@ -1,13 +1,9 @@
 package com.joneill.textstatistics.ui.main.interactor
 
-import android.graphics.BitmapFactory
 import com.joneill.textstatistics.data.preferences.PreferenceHelper
 import com.joneill.textstatistics.ui.base.interactor.BaseInteractor
 import javax.inject.Inject
-import android.content.ContentResolver
 import android.content.Context
-import android.net.Uri
-import com.joneill.textstatistics.data.text.Contact
 import com.joneill.textstatistics.data.text.TextDataHelper
 
 
--- app/src/main/java/com/joneill/textstatistics/ui/main/interactor/MainMVPInteractor.kt
+++ app/src/main/java/com/joneill/textstatistics/ui/main/interactor/MainMVPInteractor.kt
@@ -1,6 +1,5 @@
 package com.joneill.textstatistics.ui.main.interactor
 
-import com.joneill.textstatistics.data.text.Contact
 import com.joneill.textstatistics.ui.base.interactor.MVPInteractor
 
 interface MainMVPInteractor : MVPInteractor {
