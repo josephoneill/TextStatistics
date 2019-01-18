784687e79659b547787d6b24cb59eee2c95e2288
josephoneill <devjoseph97@gmail.com>
2019-01-17 23:49:17 -0500

    Added method for tab selection through TabLayout

--- app/src/main/java/com/joneill/textstatistics/ui/home/presenter/HomeMVPPresenter.kt
+++ app/src/main/java/com/joneill/textstatistics/ui/home/presenter/HomeMVPPresenter.kt
@@ -3,10 +3,11 @@ package com.joneill.textstatistics.ui.main.presenter
 import com.joneill.textstatistics.ui.base.presenter.MVPPresenter
 import com.joneill.textstatistics.ui.home.view.HomeMVPView
 import com.joneill.textstatistics.ui.main.interactor.HomeMVPInteractor
-import com.joneill.textstatistics.data.text.Contact
+import com.joneill.textstatistics.data.text.data.Contact
 
 
 interface HomeMVPPresenter<V : HomeMVPView, I : HomeMVPInteractor> : MVPPresenter<V, I> {
     fun onViewPrepared()
     fun onContactItemClick(contact : Contact)
+    fun onTabSelected(pos : Int)
 }
--- app/src/main/java/com/joneill/textstatistics/ui/home/presenter/HomePresenter.kt
+++ app/src/main/java/com/joneill/textstatistics/ui/home/presenter/HomePresenter.kt
@@ -1,24 +1,51 @@
 package com.joneill.textstatistics.ui.main.presenter
 
-import com.joneill.textstatistics.data.text.Contact
+import android.util.Log
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.utils.Utils
+import com.joneill.textstatistics.data.text.data.Contact
 import com.joneill.textstatistics.ui.base.presenter.BasePresenter
 import com.joneill.textstatistics.ui.home.view.HomeMVPView
 import com.joneill.textstatistics.ui.main.interactor.HomeMVPInteractor
 import javax.inject.Inject
 import com.google.gson.Gson
+import com.joneill.textstatistics.util.CommonUtil
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 open class HomePresenter<V : HomeMVPView, I : HomeMVPInteractor> @Inject internal constructor(interactor: I) : BasePresenter<V, I>(interactor = interactor), HomeMVPPresenter<V, I> {
 
     override fun onViewPrepared() {
         interactor!!.loadData().subscribe {
-            getConversations()
+            onDataLoaded()
         }
     }
 
-    private fun getConversations() = interactor?.let { it ->
+    private fun onDataLoaded() = interactor?.let { it ->
         val contacts = it.getContacts()
         getView()?.displayContactsList(contacts)
+
+        loadChart("Texts Sent (Weekly)", 7, false)
+    }
+
+    private fun loadChart(title : String, days : Long, animateX : Boolean) = interactor?.let {
+        var messages = it.getMessages()
+        var entries : MutableList<Pair<String, Entry>> = mutableListOf()
+        messages = it.getMessagesInDateRange(messages, CommonUtil.getDateXDaysAgo(days), Date(System.currentTimeMillis()))
+        val counts = it.getMessageCountByDate(messages)
+
+        for (i in 0 until days) {
+            val date = CommonUtil.getDateXDaysAgo(days - 1 - i)
+            val name = SimpleDateFormat("MM/dd").format(date)
+            var count : Float? = counts[name]?.toFloat()
+            if (count == null) {
+                count = 0.0f
+            }
+            entries.add(Pair(name, Entry(i.toFloat(), count!!)))
+        }
+
+        getView()?.showChartCard(title, messages.count().toString(), entries, animateX)
     }
 
     override fun onContactItemClick(contact : Contact) {
@@ -26,4 +53,14 @@ open class HomePresenter<V : HomeMVPView, I : HomeMVPInteractor> @Inject interna
         interactor!!.setCurrentContact(gson.toJson(contact))
         getView()?.openContactDataFragment(contact)
     }
+
+    override fun onTabSelected(pos : Int) {
+        if (pos == 0) {
+            loadChart("Texts Sent (Weekly)", 7, false)
+        } else {
+            loadChart("Texts Sent (Monthly)", 30, true)
+        }
+    }
+
+
 }
