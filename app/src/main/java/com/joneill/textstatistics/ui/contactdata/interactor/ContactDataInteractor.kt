e18b68ed94d4b81c310992f91aa1ea4dc86d4338
josephoneill <devjoseph97@gmail.com>
2019-01-17 23:46:07 -0500

    Added new Message methods

--- app/src/main/java/com/joneill/textstatistics/ui/contactdata/interactor/ContactDataInteractor.kt
+++ app/src/main/java/com/joneill/textstatistics/ui/contactdata/interactor/ContactDataInteractor.kt
@@ -5,16 +5,26 @@ import com.joneill.textstatistics.ui.base.interactor.BaseInteractor
 import javax.inject.Inject
 import android.content.Context
 import com.joneill.textstatistics.data.repository.MessagesRepository
-import com.joneill.textstatistics.data.text.Contact
-import com.joneill.textstatistics.data.text.Message
-import com.joneill.textstatistics.data.text.TextDataHelper
+import com.joneill.textstatistics.data.text.AppTextDataHelper
+import com.joneill.textstatistics.data.text.data.Contact
+import com.joneill.textstatistics.data.text.data.Message
+import java.util.*
 
 
-class ContactDataInteractor @Inject internal constructor(preferenceHelper: PreferenceHelper, context : Context, private val messagesRepository: MessagesRepository) : BaseInteractor(preferenceHelper = preferenceHelper), ContactDataMVPInteractor {
+class ContactDataInteractor @Inject internal constructor(preferenceHelper: PreferenceHelper, private val textDataHelper: AppTextDataHelper, private val messagesRepository: MessagesRepository) : BaseInteractor(preferenceHelper = preferenceHelper), ContactDataMVPInteractor {
 
-    override fun getMessagesByContact(contact: Contact): List<Message> = messagesRepository.getMessagesByContact(contact)
+    override fun getMessages(): List<Message> = messagesRepository.messages
+
+    override fun getMessagesByDate(messages : List<Message>, date: Date): List<Message> = messagesRepository.getMessagesByDate(messages, date)
+
+    override fun getMessagesByContact(messages : List<Message>, contact: Contact): List<Message> = textDataHelper.getMessagesByContact(messages, contact)
 
     override fun getCurrentContact(): String? = preferenceHelper.getCurrentContact()
+
+    override fun getMessagesInDateRange(messages: List<Message>, startDate: Date, endDate: Date): List<Message> = textDataHelper.getMessagesInDateRange(messages, startDate, endDate)
+
+    override fun getMessageCountOnDate(messages: List<Message>, date: Date): Int = textDataHelper.getMessageCountOnDate(messages, date)
+
     /*override fun filterCreatorsContactsOnly(contacts : List<Contact>?): List<Contact>? {
         return textDataHelper.filterCreatorsContactsOnly(contacts)
     }*/
--- app/src/main/java/com/joneill/textstatistics/ui/contactdata/interactor/ContactDataMVPInteractor.kt
+++ app/src/main/java/com/joneill/textstatistics/ui/contactdata/interactor/ContactDataMVPInteractor.kt
@@ -1,10 +1,15 @@
 package com.joneill.textstatistics.ui.main.interactor
 
-import com.joneill.textstatistics.data.text.Contact
-import com.joneill.textstatistics.data.text.Message
+import com.joneill.textstatistics.data.text.data.Contact
+import com.joneill.textstatistics.data.text.data.Message
 import com.joneill.textstatistics.ui.base.interactor.MVPInteractor
+import java.util.*
 
 interface ContactDataMVPInteractor : MVPInteractor {
-    fun getMessagesByContact(contact : Contact) : List<Message>
+    fun getMessages() : List<Message>
+    fun getMessagesByContact(messages : List<Message>, contact : Contact) : List<Message>
+    fun getMessagesByDate(messages : List<Message>, date : Date) : List<Message>
+    fun getMessageCountOnDate(messages : List<Message>, date : Date) : Int
+    fun getMessagesInDateRange(messages : List<Message>, startDate : Date, endDate : Date) : List<Message>
     fun getCurrentContact() : String?
 }
