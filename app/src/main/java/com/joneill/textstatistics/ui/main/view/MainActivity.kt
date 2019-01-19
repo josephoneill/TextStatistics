339e3115bc918f1732feacb569d8992ca29a712e
josephoneill <devjoseph97@gmail.com>
2019-01-18 20:26:31 -0500

    Added methods to load Settings

--- app/src/main/java/com/joneill/textstatistics/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/joneill/textstatistics/ui/main/view/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.joneill.textstatistics.ui.main.view
 
+import android.app.Activity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.joneill.textstatistics.R
@@ -8,14 +11,16 @@ import com.joneill.textstatistics.ui.base.view.BaseActivity
 import com.joneill.textstatistics.ui.home.view.HomeFragment
 import com.joneill.textstatistics.ui.main.interactor.MainMVPInteractor
 import com.joneill.textstatistics.ui.main.presenter.MainMVPPresenter
+import com.joneill.textstatistics.ui.settings.view.SettingsFragment
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.support.HasSupportFragmentInjector
 
 import javax.inject.Inject
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : BaseActivity(), MainMVPView, HasSupportFragmentInjector {
-
     @Inject
     internal lateinit var presenter: MainMVPPresenter<MainMVPView, MainMVPInteractor>
     @Inject
@@ -24,8 +29,28 @@ class MainActivity : BaseActivity(), MainMVPView, HasSupportFragmentInjector {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         presenter.onAttach(this)
+        loadTheme()
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+    }
+
+    private fun loadTheme() {
+        presenter.getCustomTheme()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_toolbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.action_open_settings -> {
+                presenter.onSettingsMenuClicked()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onStart() {
@@ -34,7 +59,14 @@ class MainActivity : BaseActivity(), MainMVPView, HasSupportFragmentInjector {
 
     override fun loadHomeFragment() {
         val transaction : FragmentTransaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.frame_home_fragment_container, HomeFragment())
+        transaction.replace(R.id.main_fragment_container, HomeFragment())
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+    override fun loadSettingsFragment() {
+        val transaction : FragmentTransaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.main_fragment_container, SettingsFragment())
         transaction.addToBackStack(null)
         transaction.commit()
     }
@@ -57,13 +89,23 @@ class MainActivity : BaseActivity(), MainMVPView, HasSupportFragmentInjector {
 
     }
 
-
     override fun getViewActivity() = this
 
 
     override fun onRequestPermissionsResult(requestCode: Int,
                                             permissions: Array<String>, grantResults: IntArray) {
         presenter.onPermissionsResult()
+    }
+
+    override fun setCustomTheme(isDarkTheme: Boolean) {
+        Log.e("Ran", "joneill running running ran");
+        if(isDarkTheme) {
+            Log.e("joneill ", "joneill let use dark")
+            setTheme(R.style.AppTheme)
+        } else {
+            Log.e("joneill ", "joneill lets not use dark")
+            setTheme(R.style.LightTheme)
+        }
     }
 
     override fun supportFragmentInjector() = dispatchingAndroidInjector
--- app/src/main/java/com/joneill/textstatistics/ui/main/view/MainMVPView.kt
+++ app/src/main/java/com/joneill/textstatistics/ui/main/view/MainMVPView.kt
@@ -4,6 +4,8 @@ import android.app.Activity
 import com.joneill.textstatistics.ui.base.view.MVPView
 
 interface MainMVPView : MVPView {
+    fun setCustomTheme(isDarkTheme : Boolean)
     fun getViewActivity() : Activity
     fun loadHomeFragment()
+    fun loadSettingsFragment()
 }
