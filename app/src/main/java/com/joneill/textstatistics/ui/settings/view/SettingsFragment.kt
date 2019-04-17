e08cc4367993216eaba2fc133aa1c4ffe633d84a
josephoneill <devjoseph97@gmail.com>
2019-04-17 17:36:27 -0400

    Added methods to handle lifecycle such as onPause and onDestroy

--- app/src/main/java/com/joneill/textstatistics/ui/settings/view/SettingsFragment.kt
+++ app/src/main/java/com/joneill/textstatistics/ui/settings/view/SettingsFragment.kt
@@ -1,38 +1,36 @@
 package com.joneill.textstatistics.ui.settings.view
 
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.provider.Settings
 import androidx.fragment.app.FragmentTransaction
-import androidx.recyclerview.widget.DefaultItemAnimator
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.github.mikephil.charting.data.Entry
 import com.joneill.textstatistics.R
-import com.joneill.textstatistics.data.text.data.Contact
-import com.joneill.textstatistics.ui.base.view.BaseFragment
-import com.joneill.textstatistics.ui.home.view.adapter.ContactsAdapter
-import com.joneill.textstatistics.ui.home.view.adapter.OnContactItemClickListener
-import com.joneill.textstatistics.ui.main.interactor.HomeMVPInteractor
-import com.joneill.textstatistics.ui.main.presenter.HomeMVPPresenter
-import kotlinx.android.synthetic.main.fragment_home.*
-import javax.inject.Inject
-import javax.inject.Provider
-import com.github.mikephil.charting.components.XAxis
-import com.github.mikephil.charting.formatter.IAxisValueFormatter
-import android.graphics.Color
-import androidx.core.content.res.ResourcesCompat
 import androidx.preference.PreferenceFragmentCompat
-import com.github.mikephil.charting.animation.Easing
-import com.github.mikephil.charting.data.LineDataSet
-import com.github.mikephil.charting.data.LineData
-import com.google.android.material.tabs.TabLayout
-import com.joneill.textstatistics.util.CommonUtil
-import com.joneill.textstatistics.R.attr.*
 
 
-class SettingsFragment : PreferenceFragmentCompat() {
+
+
+class SettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        preferenceScreen.sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+    }
+
+    override fun onPause() {
+        super.onPause()
+        preferenceScreen.sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    override fun onSharedPreferenceChanged(p0: SharedPreferences?, p1: String?) {
+        activity?.recreate()
     }
 }
