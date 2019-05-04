36d3aab0cdafd5d2ea92814801ad9cb03c79958a
josephoneill <devjoseph97@gmail.com>
2019-05-04 15:40:56 -0400

    removes necessary function displayContactsList()

--- app/src/main/java/com/joneill/textstatistics/ui/home/view/HomeMVPView.kt
+++ app/src/main/java/com/joneill/textstatistics/ui/home/view/HomeMVPView.kt
@@ -2,11 +2,9 @@ package com.joneill.textstatistics.ui.home.view
 
 import com.github.mikephil.charting.data.Entry
 import com.joneill.textstatistics.data.text.data.Contact
-import com.joneill.textstatistics.data.text.data.Message
 import com.joneill.textstatistics.ui.base.view.MVPView
 
 interface HomeMVPView : MVPView {
-    fun displayContactsList(contacts: List<Contact>?) : Unit?
     fun displayTopContactsList(list : List<Pair<Contact?, Int>>)
     fun showDashboard()
     fun showChartCard(title : String, dataValue : String, dataList: List<Pair<String, Entry>>, animateX : Boolean)
