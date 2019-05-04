f98708c0efa5cfb16cea88965ffe74b3ca8bbda5
josephoneill <devjoseph97@gmail.com>
2019-05-04 15:38:21 -0400

    implements new setOnClickListener() function in the ViewHolder

--- app/src/main/java/com/joneill/textstatistics/ui/home/view/adapter/TopContactsAdapter.kt
+++ app/src/main/java/com/joneill/textstatistics/ui/home/view/adapter/TopContactsAdapter.kt
@@ -6,17 +6,19 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.joneill.textstatistics.R
 import com.joneill.textstatistics.data.text.data.Contact
+import com.joneill.textstatistics.ui.home.interactor.HomeMVPInteractor
 import com.joneill.textstatistics.ui.home.presenter.adapter.TopContactsMVPPresenter
-import com.joneill.textstatistics.ui.main.interactor.HomeMVPInteractor
 import kotlinx.android.synthetic.main.contacts_list.view.*
 
 
-class TopContactsAdapter(private var presenter : TopContactsMVPPresenter<TopContactsMVPView, HomeMVPInteractor>?,
-                         private val map : MutableList<Pair<Contact, Int>>) : RecyclerView.Adapter<TopContactsAdapter.TopContactsViewHolder>() {
+class TopContactsAdapter(private var presenter: TopContactsMVPPresenter<TopContactsMVPView, HomeMVPInteractor>?) : RecyclerView.Adapter<TopContactsAdapter.TopContactsViewHolder>() {
+    var onRecyclerItemClickListener: OnContactItemClickListener? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TopContactsViewHolder {
-        return TopContactsViewHolder(LayoutInflater.from(parent.context)
+        val viewHolder = TopContactsViewHolder(presenter, LayoutInflater.from(parent.context)
                 .inflate(R.layout.contacts_list, parent, false))
+        presenter?.onAttach(viewHolder)
+        return viewHolder
     }
 
     override fun onBindViewHolder(holder: TopContactsViewHolder, position: Int) {
@@ -28,20 +30,36 @@ class TopContactsAdapter(private var presenter : TopContactsMVPPresenter<TopCont
         return presenter!!.getItemCount()
     }
 
-    fun setPresenter(presenter : TopContactsMVPPresenter<TopContactsMVPView, HomeMVPInteractor>) {
+    fun setPresenter(presenter: TopContactsMVPPresenter<TopContactsMVPView, HomeMVPInteractor>) {
         this.presenter = presenter
     }
 
-    fun setContactsList(map : List<Pair<Contact?, Int>>) {
+    fun setContactsList(map: List<Pair<Contact?, Int>>) {
         presenter?.setContactsList(map)
         notifyDataSetChanged()
     }
 
-    inner class TopContactsViewHolder(view: View) : RecyclerView.ViewHolder(view), TopContactsMVPView {
-
-        override fun setContactName(name : String?) {
-            itemView.text_contact_name_card.text = name
+    inner class TopContactsViewHolder(private val presenter: TopContactsMVPPresenter<TopContactsMVPView, HomeMVPInteractor>?, view: View) : RecyclerView.ViewHolder(view), TopContactsMVPView {
+        override fun setOnClickListener() {
+            itemView.layout_contact_card_holder.setOnClickListener {
+                presenter?.onContactClicked(adapterPosition)
+            }
         }
 
+        override fun onClick(contact: Contact) {
+            onRecyclerItemClickListener?.onItemClick(contact)
+        }
+
+        override fun setContactName(name: String?) {
+            name?.let {
+                itemView.text_name_contact_card.text = it
+            }
+        }
+
+        override fun setContactMessageCount(count: Int) {
+            count.toString().let {
+                itemView.text_message_count_contact_card.text = this.itemView.resources.getString(R.string.messages_past_30_days, it)
+            }
+        }
     }
 }
