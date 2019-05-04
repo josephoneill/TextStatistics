65af6cad9c69f2f481342dbd0b249143b91396fd
josephoneill <devjoseph97@gmail.com>
2019-05-04 15:35:28 -0400

    implements new function onContactClicked()

--- app/src/main/java/com/joneill/textstatistics/ui/home/presenter/adapter/TopContactsPresenter.kt
+++ app/src/main/java/com/joneill/textstatistics/ui/home/presenter/adapter/TopContactsPresenter.kt
@@ -2,8 +2,8 @@ package com.joneill.textstatistics.ui.home.presenter.adapter
 
 import com.joneill.textstatistics.data.text.data.Contact
 import com.joneill.textstatistics.ui.base.presenter.BasePresenter
+import com.joneill.textstatistics.ui.home.interactor.HomeMVPInteractor
 import com.joneill.textstatistics.ui.home.view.adapter.TopContactsMVPView
-import com.joneill.textstatistics.ui.main.interactor.HomeMVPInteractor
 import javax.inject.Inject
 
 open class TopContactsPresenter<V : TopContactsMVPView, I : HomeMVPInteractor> @Inject internal constructor(interactor: I) : BasePresenter<V, I>(interactor = interactor), TopContactsMVPPresenter<V, I> {
@@ -15,8 +15,16 @@ open class TopContactsPresenter<V : TopContactsMVPView, I : HomeMVPInteractor> @
 
     override fun onBind(holder: TopContactsMVPView, position: Int) {
         when {
-            contactsList.isNotEmpty() -> holder.setContactName(contactsList[position].first?.name)
+            contactsList.isNotEmpty() -> {
+                holder.setContactName(contactsList[position].first?.name)
+                holder.setContactMessageCount(contactsList[position].second)
+            }
         }
+        holder.setOnClickListener()
+    }
+    override fun onContactClicked(position : Int) {
+        val contact = contactsList[position]
+        getView()?.onClick(contact.first!!)
     }
 
     override fun getItemCount() : Int = contactsList.size
