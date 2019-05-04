447535713521b3d2bc602687f8633dba4c53ab91
josephoneill <devjoseph97@gmail.com>
2019-05-04 15:33:14 -0400

    adds new function loadChart()

--- app/src/main/java/com/joneill/textstatistics/ui/contactdata/presenter/ContactDataPresenter.kt
+++ app/src/main/java/com/joneill/textstatistics/ui/contactdata/presenter/ContactDataPresenter.kt
@@ -1,10 +1,15 @@
-package com.joneill.textstatistics.ui.main.presenter
+package com.joneill.textstatistics.ui.contactdata.presenter
 
+import com.github.mikephil.charting.data.Entry
 import com.google.gson.Gson
 import com.joneill.textstatistics.data.text.data.Contact
+import com.joneill.textstatistics.data.text.data.Message
 import com.joneill.textstatistics.ui.base.presenter.BasePresenter
-import com.joneill.textstatistics.ui.home.view.ContactDataMVPView
-import com.joneill.textstatistics.ui.main.interactor.ContactDataMVPInteractor
+import com.joneill.textstatistics.ui.contactdata.interactor.ContactDataMVPInteractor
+import com.joneill.textstatistics.ui.contactdata.view.ContactDataMVPView
+import com.joneill.textstatistics.util.CommonUtil
+import java.text.SimpleDateFormat
+import java.util.*
 import javax.inject.Inject
 
 
@@ -24,5 +29,26 @@ open class ContactDataPresenter<V : ContactDataMVPView, I : ContactDataMVPIntera
         messages = it.getMessagesByContact(messages, contact)
         //messages = it.getMessagesInDateRange(messages, Date(System.currentTimeMillis() - 7L * 24 * 3600 * 1000), Date(System.currentTimeMillis()))
         getView()?.displayMessagesList(messages)
+        loadChart("", 30, true, messages)
+    }
+
+    private fun loadChart(title: String, days: Long, animateX: Boolean, data : List<Message>) = interactor?.let {
+        var messages = data
+        val entries: MutableList<Pair<String, Entry>> = mutableListOf()
+        messages = it.getMessagesInDateRange(messages, CommonUtil.getDateXDaysAgo(days), Date(System.currentTimeMillis()))
+        val counts = it.getMessageCountByDate(messages)
+
+        for (i in 0 until days) {
+            val date = CommonUtil.getDateXDaysAgo(days - 1 - i)
+            val name = SimpleDateFormat("MM/dd").format(date)
+            var count: Float? = counts[name]?.toFloat()
+            if (count == null) {
+
+                count = 0.0f
+            }
+            entries.add(Pair(name, Entry(i.toFloat(), count)))
+        }
+
+        getView()?.showChartCard(title, messages.count().toString(), entries, animateX)
     }
 }
