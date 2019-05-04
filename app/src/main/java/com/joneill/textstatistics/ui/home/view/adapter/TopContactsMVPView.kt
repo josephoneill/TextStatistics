fff1d6141a93147fef7e6f5d65e075edd37ccf0c
josephoneill <devjoseph97@gmail.com>
2019-05-04 15:39:25 -0400

    implements new functions for onClick actions

--- app/src/main/java/com/joneill/textstatistics/ui/home/view/adapter/TopContactsMVPView.kt
+++ app/src/main/java/com/joneill/textstatistics/ui/home/view/adapter/TopContactsMVPView.kt
@@ -4,5 +4,8 @@ import com.joneill.textstatistics.data.text.data.Contact
 import com.joneill.textstatistics.ui.base.view.MVPView
 
 interface TopContactsMVPView : MVPView {
+    fun setOnClickListener()
     fun setContactName(name : String?)
+    fun setContactMessageCount(count : Int)
+    fun onClick(contact: Contact)
 }
