1e5d6b6baf1df64a55909197eb4b85ad01a6db96
josephoneill <devjoseph97@gmail.com>
2019-05-06 23:55:27 -0400

    switches from Lists of Pairs to using custom data classes for the graph entries

--- app/src/main/java/com/joneill/textstatistics/ui/contactdata/view/ContactDataMVPView.kt
+++ app/src/main/java/com/joneill/textstatistics/ui/contactdata/view/ContactDataMVPView.kt
@@ -1,10 +1,10 @@
 package com.joneill.textstatistics.ui.contactdata.view
 
-import com.github.mikephil.charting.data.Entry
+import com.joneill.textstatistics.data.graph.ComparisonEntrySet
 import com.joneill.textstatistics.data.text.data.Message
 import com.joneill.textstatistics.ui.base.view.MVPView
 
 interface ContactDataMVPView : MVPView {
     fun displayMessagesList(messages: List<Message>?) : Unit?
-    fun showChartCard(title : String, dataValue : String, dataList: List<Pair<String, Entry>>, animateX : Boolean)
+    fun showChartCard(title : String, dataValue : String, comparisonsList: List<ComparisonEntrySet>, animateX : Boolean)
 }
