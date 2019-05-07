2d9913c1c0264822bbe6af0f3660a892526ee5f1
josephoneill <devjoseph97@gmail.com>
2019-05-06 23:52:50 -0400

    adds data classes for the message count per date entry

---
+++ app/src/main/java/com/joneill/textstatistics/data/graph/CountedDateMessageEntry.kt
@@ -0,0 +1,5 @@
+package com.joneill.textstatistics.data.graph
+
+import com.github.mikephil.charting.data.Entry
+
+data class CountedDateMessageEntry constructor(val date : String, val countEntry: Entry)
