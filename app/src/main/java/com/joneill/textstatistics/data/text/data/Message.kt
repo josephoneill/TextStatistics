f3a64a88465bab2dd4f98a7daf8fcbd4283f7b32
josephoneill <devjoseph97@gmail.com>
2019-05-06 23:53:14 -0400

    adds boolean value to determine whether message was sent by the user

--- app/src/main/java/com/joneill/textstatistics/data/text/data/Message.kt
+++ app/src/main/java/com/joneill/textstatistics/data/text/data/Message.kt
@@ -1,4 +1,4 @@
 package com.joneill.textstatistics.data.text.data
 import java.util.*
 
-data class Message constructor(val data: String?, val contact : Contact?, val date : Date)
+data class Message constructor(val data: String?, val contact : Contact?, val date : Date, val sent : Boolean)
