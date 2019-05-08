f4cb4d5f3b7d0fcb178becebc18455e4bb2f4a60
josephoneill <devjoseph97@gmail.com>
2019-05-08 19:01:04 -0400

    Add StatsChartFragmentProvider to the injector

--- app/src/main/java/com/joneill/textstatistics/di/builder/ActivityBuilder.kt
+++ app/src/main/java/com/joneill/textstatistics/di/builder/ActivityBuilder.kt
@@ -4,11 +4,12 @@ import com.joneill.textstatistics.ui.contactdata.ContactDataFragmentProvider
 import com.joneill.textstatistics.ui.home.HomeFragmentProvider
 import com.joneill.textstatistics.ui.main.MainActivityModule
 import com.joneill.textstatistics.ui.main.view.MainActivity
+import com.joneill.textstatistics.ui.stats_chart.StatsChartFragmentProvider
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
 @Module
 abstract class ActivityBuilder {
-    @ContributesAndroidInjector(modules = [(MainActivityModule::class), (HomeFragmentProvider::class), (ContactDataFragmentProvider::class)])
+    @ContributesAndroidInjector(modules = [(MainActivityModule::class), (HomeFragmentProvider::class), (ContactDataFragmentProvider::class), (StatsChartFragmentProvider::class)])
     abstract fun bindMainActivity(): MainActivity
 }
