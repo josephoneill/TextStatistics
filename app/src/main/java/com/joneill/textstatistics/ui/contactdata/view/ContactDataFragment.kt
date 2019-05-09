109e7f5b7c4ea8ec5fabbcc74524ed26152b246f
josephoneill <devjoseph97@gmail.com>
2019-05-09 02:40:09 -0400

    Change comparisonList into a list of Entry, providing the xAxisLabels as a second list

--- app/src/main/java/com/joneill/textstatistics/ui/contactdata/view/ContactDataFragment.kt
+++ app/src/main/java/com/joneill/textstatistics/ui/contactdata/view/ContactDataFragment.kt
@@ -6,16 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineDataSet
 import com.github.mikephil.charting.formatter.IAxisValueFormatter
 import com.joneill.textstatistics.R
-import com.joneill.textstatistics.data.graph.ComparisonEntrySet
 import com.joneill.textstatistics.data.text.data.Message
 import com.joneill.textstatistics.ui.base.view.BaseFragment
 import com.joneill.textstatistics.ui.contactdata.interactor.ContactDataMVPInteractor
 import com.joneill.textstatistics.ui.contactdata.presenter.ContactDataMVPPresenter
 import kotlinx.android.synthetic.main.fragment_contact_data.*
+import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.tabbed_themed_graph_card.view.*
 import javax.inject.Inject
 import javax.inject.Provider
@@ -39,7 +38,7 @@ class ContactDataFragment : BaseFragment(), ContactDataMVPView {
         super.onViewCreated(view, savedInstanceState)
     }
 
-    override fun setUp() {
+    override fun init() {
         contact_data_recycler_view.layoutManager = layoutManager.get()
         contact_data_recycler_view.itemAnimator = DefaultItemAnimator()
         contact_data_recycler_view.adapter = contactDataAdapter
@@ -50,32 +49,15 @@ class ContactDataFragment : BaseFragment(), ContactDataMVPView {
         contactDataAdapter.addMessagesToList(it)
     }
 
-    override fun showChartCard(title: String, dataValue: String, comparisonsList: List<ComparisonEntrySet>, animateX: Boolean) {
+    override fun showChartCard(title: String, dataValue: String, dataSets: List<LineDataSet>, xAxisLabels : List<String>, animateX: Boolean) {
         // Set card info
         contact_data_chart_card.setTitle(title)
         contact_data_chart_card.setValue(dataValue)
 
-        val mLineChart = contact_data_chart_card.card_line_chart
-        // The labels that should be drawn on the XAxis
-        val days = ArrayList<String>()
-
-        val dataSets = ArrayList<LineDataSet>()
-
-        for (comparison in comparisonsList) {
-            val isDaysEmpty = days.size == 0
-            val dataComp = ArrayList<Entry>()
-            for (data in comparison.messageCountsByDate) {
-                if(isDaysEmpty) {
-                    days.add(data.date)
-                }
-                dataComp.add(data.countEntry)
-            }
-            val setComp = LineDataSet(dataComp, comparison.label)
-            dataSets.add(setComp)
-        }
+        val mLineChart = home_chart_card.card_line_chart
 
         val formatter = IAxisValueFormatter { value, _ ->
-            days[value.toInt()]
+            xAxisLabels[value.toInt()]
         }
 
         mLineChart.xAxis.valueFormatter = formatter
@@ -84,7 +66,7 @@ class ContactDataFragment : BaseFragment(), ContactDataMVPView {
         } else {
             mLineChart.animateY(700)
         }
-        mLineChart.addDataSet(dataSets)
-        mLineChart.invalidate() // refresh
+        mLineChart.resetData()
+        mLineChart.addDataSets(dataSets)
     }
 }
--- app/src/main/java/com/joneill/textstatistics/ui/contactdata/view/ContactDataMVPView.kt
+++ app/src/main/java/com/joneill/textstatistics/ui/contactdata/view/ContactDataMVPView.kt
@@ -1,10 +1,10 @@
 package com.joneill.textstatistics.ui.contactdata.view
 
-import com.joneill.textstatistics.data.graph.ComparisonEntrySet
+import com.github.mikephil.charting.data.LineDataSet
 import com.joneill.textstatistics.data.text.data.Message
 import com.joneill.textstatistics.ui.base.view.MVPView
 
 interface ContactDataMVPView : MVPView {
     fun displayMessagesList(messages: List<Message>?) : Unit?
-    fun showChartCard(title : String, dataValue : String, comparisonsList: List<ComparisonEntrySet>, animateX : Boolean)
+    fun showChartCard(title : String, dataValue : String, dataSets: List<LineDataSet>, xAxisLabels : List<String>, animateX : Boolean)
 }
