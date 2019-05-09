d27b3dfd4954030a6aedb8c0141205f02d1af252
josephoneill <devjoseph97@gmail.com>
2019-05-09 02:37:02 -0400

    Remove unused files

--- app/src/main/java/com/joneill/textstatistics/data/graph/ComparisonEntrySet.kt
+++
@@ -1,3 +0,0 @@
-package com.joneill.textstatistics.data.graph
-
-data class ComparisonEntrySet constructor(val messageCountsByDate: List<CountedDateMessageEntry>, val label: String)
--- app/src/main/java/com/joneill/textstatistics/data/graph/CountedDateMessageEntry.kt
+++
@@ -1,5 +0,0 @@
-package com.joneill.textstatistics.data.graph
-
-import com.github.mikephil.charting.data.Entry
-
-data class CountedDateMessageEntry constructor(val date : String, val countEntry: Entry)
